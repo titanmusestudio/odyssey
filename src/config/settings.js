7eea7a2f521fdbba60df68c8fb022468f5308033
Titan Muse Studio <titanmusestudio@gmail.com>
2025-09-20 21:29:45 -0400

    Update settings.js

--- src/config/settings.js
+++ src/config/settings.js
@@ -1,8 +1,6 @@
 export default {
-	title: `Odyssey Astro Theme | A Marketing Website Theme for Startups and Businesses`,
-	description: `A simple, clean, and modern theme for a startup or businesses' marketing website.`,
-	url: `https://odyssey-theme.sapling.supply`, // No trailing slash!
-	name: `Odyssey`, // The short name of the business or brand name. Used for things like the copyright in the footer.
+	title: `Titan Muse Studio | Where Artistry Meets Analytics.`, // No trailing slash!
+	name: `Titan Muse Studio`, // The short name of the business or brand name. Used for things like the copyright in the footer.
 	enableThemeSwitcher: true,
 	showPlug: true, // Disable this if you want to remove the plug from the footer. (╯°□°)╯︵ ┻━┻
 };
